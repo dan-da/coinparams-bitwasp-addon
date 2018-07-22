0552cce2028a37c3807e96402680918aaaab8930
danda <danda@osc.co.cr>
2018-07-22 19:47:07 +0800

    make tests compatible with bitwasp 0.0.35.1

--- tests/MultiCoinNetworkTest.test.php
+++ tests/MultiCoinNetworkTest.test.php
@@ -20,9 +20,7 @@ class MultiCoinNetworkTest extends test_base {
         $this->eq( $network->getNetMagicBytes(), 'f9beb4d9', "$chain: getNetMagicBytes");
         $this->eq( $network->getPrivByte(), '80', "$chain: getPrivByte");
         $this->eq( $network->getAddressByte(), '00', "$chain: getAddressByte");
-        $this->eq( $network->getAddressPrefixLength(), 1, "$chain: getAddressPrefixLength");
         $this->eq( $network->getP2shByte(), '05', "$chain: getP2shByte");
-        $this->eq( $network->getP2shPrefixLength(), 1, "$chain: getP2shPrefixLength");
         $this->eq( $network->getHDPubByte(), '0488b21e', "$chain: getHDPubByte");
         $this->eq( $network->getHDPrivByte(), '0488ade4', "$chain: getHDPrivByte");
         $this->eq( $network->getSegwitBech32Prefix(), 'bc', "$chain: getSegwitBech32Prefix");
