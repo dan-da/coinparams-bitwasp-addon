b5007e9559b0e42b32206599c36de0397e4cd91e
Adam <28712999+AdamSEY@users.noreply.github.com>
2018-11-28 19:15:22 +0100

    Update Internal.php
    Exception typo.

--- src/Internal.php
+++ src/Internal.php
@@ -11,16 +11,16 @@ class Internal
     /**
      * @param $array array
      * @param $key string
-     * @param bool|string $execption: type or message to throw an exeption with your message or false to return null.
+     * @param bool|string $exception: type or message to throw an exception with your message or false to return null.
      * @return null|mixed
      * @throws ArrayKeyNotFound
      */
-    public static function aget($array , $key , $execption = false)
+    public static function aget($array , $key , $exception = false)
     {
-        if (is_array($array) AND array_key_exists($key, $array) AND true == $array[$key]){
+        if (is_array($array) AND array_key_exists($key, $array)){
             return $array[$key];
-        }elseif ($execption){
-            $message = is_string($execption) ? $execption : 'Array value not found for key ' . $key;
+        }elseif ($exception){
+            $message = is_string($exception) ? $exception : 'Array value not found for key ' . $key;
             throw new ArrayKeyNotFound($message);
         }else{
             return null;
